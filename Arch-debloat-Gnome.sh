b3a1f8945616b62b03a8bcc1d642fba10ef0c761
Urasono <89348450+Urasono@users.noreply.github.com>
2025-11-30 11:04:51 -0300

    Create Arch-debloat-Gnome.sh

---
+++ Arch-debloat-Gnome.sh
@@ -0,0 +1,10 @@
+  #!/bin/bash
+
+  #Remove Packages
+  sudo pacman -Rs gnome-maps
+  sudo pacman -Rs gnome-weather
+  sudo pacman -Rs gnome-connections
+  sudo pacman -Rs gnome-music
+  sudo pacman -Rs htop
+  sudo pacman -Rs gnome-contacts
+  sudo pacman -Rs baobab
