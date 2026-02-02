7f46bee66473b0de3e87f2018098df8ef6136743
Urasono <89348450+Urasono@users.noreply.github.com>
2026-02-01 23:50:07 -0300

    Update and rename Arch-instalação-Apps.sh to Arch-ajustes-e-instalação-de-Apps.sh

---
+++ Arch-ajustes-e-instalação-de-Apps.sh
@@ -0,0 +1,98 @@
+#!/bash/bin
+
+  #check updates
+  sudo pacman -Sy
+  sudo pacman -Syu
+
+  #grub-config microcódigo
+  sudo pacman -S amd-ucode -y
+  grub-mkconfig -o /boot/grub/grub.cfg
+
+  #set keyboard
+  setxkbmap -model abnt2 -layout br
+  loadkeys br-abnt2
+
+  #pacman-contrib (empty cache weekly)
+  sudo pacman -S  pacman-contrib -y
+  systemctl enable --now paccache.timer
+
+  #update
+  sudo pacman -Sy
+
+  #remoção da mitigação split-lock
+  sudo rm -r /etc/sysctl.d/99-splitlock.conf
+
+  #Early OOM Daemon Linux
+  sudo pacman -S earlyoom -y
+  sudo systemctl enable --now earlyoom
+  echo ""EARLYOOM_ARGS="-r 0 -m 2 -M 256000 --prefer '^(Web Content|Isolated Web Co)$' --avoid '^(dnf|apt|pacman|rpm-ostree|packagekitd|gnome-shell|gnome-session-c|gnome-session-b|lightdm|sddm|sddm-helper|gdm|gdm-wayland-ses|gdm-session-wor|gdm-x-session|Xorg|Xwayland|systemd|systemd-logind|dbus-daemon|dbus-broker|cinnamon|cinnamon-sessio|kwin_x11|kwin_wayland|plasmashell|ksmserver|plasma_session|startplasma-way|sway|i3|xfce4-session|mate-session|marco|lxqt-session|openbox|cryptsetup)$" > /etc/default/earlyoom
+  systemctl restart earlyoom
+
+  #shader booster
+
+  echo "# enforce RADV vulkan implementation for AMD GPUs
+export AMD_VULKAN_ICD=RADV
+
+  # increase AMD and Intel cache size to 12GB
+export MESA_SHADER_CACHE_MAX_SIZE=12G" > .profile
+
+  # increase NVIDEA cache size to 12GB
+ # echo "# increase Nvidia shader cache size to 12GB
+#export __GL_SHADER_DISK_CACHE_SIZE=12000000000" >.profile
+
+  #optional apps#
+  #sudo pacman -S davinci-resolve -y
+  wget "https://codeberg.org/OpenRGB/OpenRGB/releases/download/release_candidate_1.0rc2/OpenRGB_1.0rc2_x86_64_0fca93e.AppImage"
+  mkdir Openrgb
+  mv *AppImage Openrgb/
+  cd Openrgb/
+  wget "https://openrgb.org/releases/release_0.9/openrgb-udev-install.sh"
+  chmod +x openrgb-udev-install.sh
+  bash openrgb-udev-install.sh
+
+  #install packages
+  sudo pacman -S linux-headers -y
+  sudo pscman -S nano -y
+  sudo pacman -S fastfetch -y
+  sudo pacman -S gdu -y
+  sudo pacman -S keepassxc -y
+  sudo pacman -S firefox -y
+  sudo pacman -S mpv -y
+  sudo pacman -S btop -y
+  sudo pacman -S gstreamer -y
+  sudo pacman -S gst-plugins-bad -y
+  sudo pacman -S gst-plugins-good -y
+  sudo pacman -S gst-plugins-base -y
+  sudo pacman -S gst-libav -y
+  sudo pacman -S gst-plugins-ugly -y
+  sudo pacman -S ffmpeg -y
+  sudo packan -S base-devel -y
+  sudo pacman -S gufw -y
+  sudo pacman -S wine -y
+  sudo pacman -S winetricks -y
+  sudo pacman -S steam -y
+  sudo pacman -S lutris -y
+  sudo pacman -S libreoffice-still -y
+  sudo pacman -S xorg -y
+  sudo pacman -S xorg-server -y
+  sudo pacman -S xorg-apps -y
+  sudo pacman -S mesa -y
+  sudo pacman -S lib32-mesa -y
+  sudo pacman -S xdg-users-dirs -y
+  sudo pacman -S flameshot -y
+  sudo pacman -S nfoview -y
+  sudo pacman -S foliate -y
+  sudo pacman -S speedtest-cli -y
+  sudo pacman -S aria2 -y
+  sudo pacman -S thunderbird -y
+  sudo pacman -S freecad -y
+  sudo pacman -S timeshift -y
+  sudo pacman -S cmus -y
+  #yay AUR
+  git clone "https://aur.archlinux.org/yay-bin.git"
+  cd yay-bin
+  makepkg -si
+
+#end
+
+echo "Instale o dnsmasq e habilite ou descomente domain-needed, bogus-priv e bind-interface em /etc/dnsmasq.conf"
