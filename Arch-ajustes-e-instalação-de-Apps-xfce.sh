d49b7939c1af7cf518e817c20e7aea0521da752c
Urasono <89348450+Urasono@users.noreply.github.com>
2026-02-03 10:25:26 -0300

    Update Arch-ajustes-e-instalação-de-Apps-xfce.sh

--- Arch-ajustes-e-instalação-de-Apps-xfce.sh
+++ Arch-ajustes-e-instalação-de-Apps-xfce.sh
@@ -37,7 +37,7 @@ export MESA_SHADER_CACHE_MAX_SIZE=12G" >> .profile
 
   # increase NVIDEA cache size to 12GB
   #echo "# increase Nvidia shader cache size to 12GB
-#export __GL_SHADER_DISK_CACHE_SIZE=12000000000" >>.profile
+#export __GL_SHADER_DISK_CACHE_SIZE=12000000000" >> .profile
   #optional apps
   #sudo pacman -S davinci-resolve -y
   wget "https://codeberg.org/OpenRGB/OpenRGB/releases/download/release_candidate_1.0rc2/OpenRGB_1.0rc2_x86_64_0fca93e.AppImage"
