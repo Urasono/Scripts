9598016a1af556a7a2a50a293302962ae991efe4
Urasono <89348450+Urasono@users.noreply.github.com>
2026-02-03 10:06:40 -0300

    Update Arch-ajustes-e-instalação-de-Apps.sh

--- Arch-ajustes-e-instalação-de-Apps.sh
+++ Arch-ajustes-e-instalação-de-Apps.sh
@@ -33,11 +33,11 @@
 export AMD_VULKAN_ICD=RADV
 
   # increase AMD and Intel cache size to 12GB
-export MESA_SHADER_CACHE_MAX_SIZE=12G" > .profile
+export MESA_SHADER_CACHE_MAX_SIZE=12G" >> .profile
 
   # increase NVIDEA cache size to 12GB
  # echo "# increase Nvidia shader cache size to 12GB
-#export __GL_SHADER_DISK_CACHE_SIZE=12000000000" > .profile
+#export __GL_SHADER_DISK_CACHE_SIZE=12000000000" >> .profile
 
   #optional apps#
   #sudo pacman -S davinci-resolve -y
