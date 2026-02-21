a2e0b01339fbd26199de28e1ed013bb21c6f6cff
Urasono <89348450+Urasono@users.noreply.github.com>
2026-02-21 00:48:29 -0300

    Update Arch-ajustes-e-instalação-de-Apps-PT-BR-XFCE.sh

--- Arch-ajustes-e-instalação-de-Apps-PT-BR-XFCE.sh
+++ Arch-ajustes-e-instalação-de-Apps-PT-BR-XFCE.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 #Ajustes e instalação de Apps xfce PT BR - Ajustes no Arch linux e instalação de aplicativos com base no sistema em Português e buscando a instalação da interface gráfica XFCE.
 
+#Elevação de usuário ao root (CUIDADO)
+su || exit
+
 #check updates
-sudo pacman -Sy
-sudo pacman -Syu
+pacman -Sy
+pacman -Syu
 pacman-key --refresh-keys
-sudo pacman -Syu archlinux-keyring
+pacman -Syu archlinux-keyring
 
 #grub-config microcódigo
-sudo pacman -S amd-ucode -s
+pacman -S amd-ucode -s
 
 #modo root
-#grub-mkconfig -o /boot/grub/grub.cfg
+update-grub || exit
 
 #set keyboard
 setxkbmap -model abnt2 -layout br
 loadkeys br-abnt2
 
 #pacman-contrib (empty cache weekly)
-sudo pacman -S pacman-contrib -s
-systemctl enable paccache.timer
-systemctl start paccache.timer
+pacman -S pacman-contrib -s
+systemctl enable paccache.timer || exit
+systemctl start paccache.timer || exit
 
 #update
-sudo pacman -Sy
+pacman -Sy
 
 #remoção da mitigação split-lock
-#sudo rm -r /etc/sysctl.d/99-splitlock.conf
+echo "kernel.split_lock_mitigate=0" > /etc/sysctl.d/99-splitlock.conf || exit
 
 #Earlyoom Daemon Linux
-sudo pacman -S earlyoom -s
-sudo systemctl enable earlyoom
-sudo systemctl start earlyoom
-#sudo echo ""EARLYOOM_ARGS="-r 0 -m 2 -M 256000 --prefer '^(Web Content|Isolated Web Co)$' --avoid '^(dnf|apt|pacman|rpm-ostree|packagekitd|gnome-shell|gnome-session-c|gnome-session-b|lightdm|sddm|sddm-helper|gdm|gdm-wayland-ses|gdm-session-wor|gdm-x-session|Xorg|Xwayland|systemd|systemd-logind|dbus-daemon|dbus-broker|cinnamon|cinnamon-sessio|kwin_x11|kwin_wayland|plasmashell|ksmserver|plasma_session|startplasma-way|sway|i3|xfce4-session|mate-session|marco|lxqt-session|openbox|cryptsetup)$" > /etc/default/earlyoom
-systemctl restart earlyoom
+pacman -S earlyoom -s
+systemctl enable earlyoom || exit
+systemctl start earlyoom || exit
+# echo ""EARLYOOM_ARGS="-r 0 -m 2 -M 256000 --prefer '^(Web Content|Isolated Web Co)$' --avoid '^(dnf|apt|pacman|rpm-ostree|packagekitd|gnome-shell|gnome-session-c|gnome-session-b|lightdm|sddm|sddm-helper|gdm|gdm-wayland-ses|gdm-session-wor|gdm-x-session|Xorg|Xwayland|systemd|systemd-logind|dbus-daemon|dbus-broker|cinnamon|cinnamon-sessio|kwin_x11|kwin_wayland|plasmashell|ksmserver|plasma_session|startplasma-way|sway|i3|xfce4-session|mate-session|marco|lxqt-session|openbox|cryptsetup)$" > /etc/default/earlyoom
 
 #shader booster
 echo "# enforce RADV vulkan implementation for AMD GPUs
@@ -61,57 +63,55 @@ cd Ventoy/ || exit
 tar -xvf ./*download || exit
 rm ./*download || exit
 cd ../ || exit
-#sudo pacman -S flatpak -s
-#sudo pacman -S flatseal -s
+#pacman -S flatpak -s
+#pacman -S flatseal -s
 #flatpak update
-#sudo pacman -S davinci-resolve -s
+#pacman -S davinci-resolve -s
 
 #install packages
-sudo pacman -S linux-headers -s
-sudo pscman -S nano -s
-sudo pacman -S bitwarden -s
-sudo pacman -S gdu -s
-sudo pacman -S fastfetch -s
-sudo pacman -S keepassxc -s
-sudo pacman -S firefox -s
-sudo pacman -S mpv -s
-sudo pacman -S btop -s
-sudo pacman -S gstreamer -s
-sudo pacman -S gst-libav -s
-sudo pacman -S gst-plugins-bad -s
-sudo pacman -S gst-plugins-good -s
-sudo pacman -S gst-plugins-base -s
-sudo pacman -S gst-plugins-ugly -s
-sudo pacman -S ffmpeg -s
-sudo packan -S base-devel -s
-sudo pacman -S gufw -s
-sudo pacman -S wine -s
-sudo pacman -S winetricks -s
-sudo pacman -S steam -s
-sudo pacman -S lutris -s
-sudo pacman -S libreoffice-still -s
-sudo pacman -S xorg -s
-sudo pacman -S xorg-apps -s
-sudo pacman -S xorg-server -s
-sudo pacman -S mesa -s
-sudo pacman -S lib32-mesa -s
-sudo pacman -S xdg-users-dirs -s
-sudo pacman -S flameshot -s
-sudo pacman -S foliate -s
-sudo pacman -S speedtest-cli -s
-sudo pacman -S aria2 -s
-sudo pacman -S claws-mail -s
-sudo pacman -S freecad -s
-sudo pacman -S timeshift -s
-sudo pacman -S xfce4 -s
-sudo pacman -S xfce4-goodies -s
-sudo pacman -S cmus -s
-sudo pacman -S tldr -s
+pacman -S linux-headers -s
+pacman -S nano -s
+pacman -S bitwarden -s
+pacman -S gdu -s
+pacman -S fastfetch -s
+pacman -S keepassxc -s
+pacman -S firefox -s
+pacman -S mpv -s
+pacman -S btop -s
+pacman -S gstreamer -s
+pacman -S gst-libav -s
+pacman -S gst-plugins-bad -s
+pacman -S gst-plugins-good -s
+pacman -S gst-plugins-base -s
+pacman -S gst-plugins-ugly -s
+pacman -S ffmpeg -s
+pacman -S base-devel -s
+pacman -S gufw -s
+pacman -S wine -s
+pacman -S winetricks -s
+pacman -S steam -s
+pacman -S lutris -s
+pacman -S libreoffice-still -s
+pacman -S xorg -s
+pacman -S xorg-apps -s
+pacman -S xorg-server -s
+pacman -S mesa -s
+pacman -S lib32-mesa -s
+pacman -S xdg-users-dirs -s
+pacman -S flameshot -s
+pacman -S foliate -s
+pacman -S speedtest-cli -s
+pacman -S aria2 -s
+pacman -S claws-mail -s
+pacman -S freecad -s
+pacman -S timeshift -s
+pacman -S xfce4 -s
+pacman -S xfce4-goodies -s
+pacman -S cmus -s
+pacman -S tldr -s
 
 #yay AUR
 git clone "https://aur.archlinux.org/yay-bin.git"
-cd yay-bin || exit
-makepkg -si || exit
 
 #end
 echo "Instale o dnsmasq e habilite ou descomente domain-needed, bogus-priv e bind-interface em /etc/dnsmasq.conf | Reinicie o sistema, amigão"
