d65d85a224421692a095d0ff26595803d359181f
Urasono <89348450+Urasono@users.noreply.github.com>
2026-02-21 00:25:46 -0300

    Update Arch-ajustes-e-instalação-de-Apps.sh

--- Arch-ajustes-e-instalação-de-Apps.sh
+++ Arch-ajustes-e-instalação-de-Apps.sh
@@ -1,54 +1,58 @@
 #!/bin/bash
 #Ajustes e instalação de Apps - Instalação e ajustes no Arch linux com base no sistema em inglês.
 
+#Elevação de usuário ao root (CUIDADO)
+su || exit
+
 #check updates
-sudo pacman -Sy
-sudo pacman -Syu
+
+pacman -Sy
+pacman -Syu
 pacman-key --refresh-keys
-sudo pacman -Syu archlinux-keyring -y
+pacman -Syu archlinux-keyring -y
 
 #grub-config microcódigo
-sudo pacman -S amd-ucode -y
+pacman -S amd-ucode -y
 
-#Modo root
-#grub-mkconfig -o /boot/grub/grub.cfg
+#grub update
+update-grub || exit
 
 #set keyboard
 setxkbmap -model abnt2 -layout br
 loadkeys br-abnt2
 
 #pacman-contrib (empty cache weekly)
-sudo pacman -S pacman-contrib -y
-sudo systemctl enable paccache.timer
-sudo systemctl start paccache.timer
+pacman -S pacman-contrib -y
+systemctl enable paccache.timer || exit
+systemctl start paccache.timer || exit
 
 #update
-sudo pacman -Sy
+pacman -Sy
 
 #remoção da mitigação split-lock
-#sudo rm -r /etc/sysctl.d/99-splitlock.conf
+echo "kernel.split_lock_mitigate=0" > /etc/sysctl.d/99-splitlock.conf
 
 #Earlyoom Daemon Linux
-sudo pacman -S earlyoom -y
-sudo systemctl enable earlyoom
-sudo systemctl start earlyoom
-#sudo echo ""EARLYOOM_ARGS="-r 0 -m 2 -M 256000 --prefer '^(Web Content|Isolated Web Co)$' --avoid '^(dnf|apt|pacman|rpm-ostree|packagekitd|gnome-shell|gnome-session-c|gnome-session-b|lightdm|sddm|sddm-helper|gdm|gdm-wayland-ses|gdm-session-wor|gdm-x-session|Xorg|Xwayland|systemd|systemd-logind|dbus-daemon|dbus-broker|cinnamon|cinnamon-sessio|kwin_x11|kwin_wayland|plasmashell|ksmserver|plasma_session|startplasma-way|sway|i3|xfce4-session|mate-session|marco|lxqt-session|openbox|cryptsetup)$" > /etc/default/earlyoom
-systemctl restart earlyoom
+pacman -S earlyoom -y
+systemctl enable earlyoom
+systemctl start earlyoom
+#echo ""EARLYOOM_ARGS="-r 0 -m 2 -M 256000 --prefer '^(Web Content|Isolated Web Co)$' --avoid '^(dnf|apt|pacman|rpm-ostree|packagekitd|gnome-shell|gnome-session-c|gnome-session-b|lightdm|sddm|sddm-helper|gdm|gdm-wayland-ses|gdm-session-wor|gdm-x-session|Xorg|Xwayland|systemd|systemd-logind|dbus-daemon|dbus-broker|cinnamon|cinnamon-sessio|kwin_x11|kwin_wayland|plasmashell|ksmserver|plasma_session|startplasma-way|sway|i3|xfce4-session|mate-session|marco|lxqt-session|openbox|cryptsetup)$" > /etc/default/earlyoom
+#systemctl restart earlyoom
 
 #shader booster
 echo "# enforce RADV vulkan implementation for AMD GPUs
 export AMD_VULKAN_ICD=RADV
 
-  # increase AMD and Intel cache size to 12GB
+ # increase AMD and Intel cache size to 12GB
 export MESA_SHADER_CACHE_MAX_SIZE=12G" >> .profile
 
-  # increase NVIDEA cache size to 12GB
+# increase NVIDEA cache size to 12GB
 # echo "# increase Nvidia shader cache size to 12GB
 #export __GL_SHADER_DISK_CACHE_SIZE=12000000000" >> .profile
 
 #optional apps#
 
-#sudo pacman -S davinci-resolve -y
+#pacman -S davinci-resolve -y
 wget "https://codeberg.org/OpenRGB/OpenRGB/releases/download/release_candidate_1.0rc2/OpenRGB_1.0rc2_x86_64_0fca93e.AppImage"
 mkdir Openrgb || exit
 mv ./*AppImage Openrgb/ || exit
@@ -65,52 +69,50 @@ tar -xvf ./*download || exit
 rm ./*download || exit
 cd ../ || exit
 
-#sudo pacman -S flatpak -y
-#sudo pacman -S flatseal -y
+#pacman -S flatpak -y
+#pacman -S flatseal -y
 #flatpak update
 
 #install packages
-sudo pacman -S linux-headers -y
-sudo pscman -S nano -y
-sudo pacman -S bitwarden -y
-sudo pacman -S fastfetch -y
-sudo pacman -S gdu -y
-sudo pacman -S keepassxc -y
-sudo pacman -S firefox -y
-sudo pacman -S mpv -y
-sudo pacman -S btop -y
-sudo pacman -S gstreamer -y
-sudo pacman -S gst-plugins-bad -y
-sudo pacman -S gst-plugins-good -y
-sudo pacman -S gst-plugins-base -y
-sudo pacman -S gst-libav -y
-sudo pacman -S gst-plugins-ugly -y
-sudo pacman -S ffmpeg -y
-sudo packan -S base-devel -y
-sudo pacman -S gufw -y
-sudo pacman -S wine -y
-sudo pacman -S winetricks -y
-sudo pacman -S steam -y
-sudo pacman -S lutris -y
-sudo pacman -S libreoffice-still -y
-sudo pacman -S xorg -y
-sudo pacman -S mesa -y
-sudo pacman -S lib32-mesa -y
-sudo pacman -S xdg-users-dirs -y
-sudo pacman -S flameshot -y
-sudo pacman -S tldr -y
-sudo pacman -S foliate -y
-sudo pacman -S speedtest-cli -y
-sudo pacman -S aria2 -y
-sudo pacman -S claws-mail -y
-sudo pacman -S freecad -y
-sudo pacman -S timeshift -y
-sudo pacman -S cmus -y
+ pacman -S linux-headers -y
+ pacman -S nano -y
+ pacman -S bitwarden -y
+ pacman -S fastfetch -y
+ pacman -S gdu -y
+ pacman -S keepassxc -y
+ pacman -S firefox -y
+ pacman -S mpv -y
+ pacman -S btop -y
+ pacman -S gstreamer -y
+ pacman -S gst-plugins-bad -y
+ pacman -S gst-plugins-good -y
+ pacman -S gst-plugins-base -y
+ pacman -S gst-libav -y
+ pacman -S gst-plugins-ugly -y
+ pacman -S ffmpeg -y
+ pacman -S base-devel -y
+ pacman -S gufw -y
+ pacman -S wine -y
+ pacman -S winetricks -y
+ pacman -S steam -y
+ pacman -S lutris -y
+ pacman -S libreoffice-still -y
+ pacman -S xorg -y
+ pacman -S mesa -y
+ pacman -S lib32-mesa -y
+ pacman -S xdg-users-dirs -y
+ pacman -S flameshot -y
+ pacman -S tldr -y
+ pacman -S foliate -y
+ pacman -S speedtest-cli -y
+ pacman -S aria2 -y
+ pacman -S claws-mail -y
+ pacman -S freecad -y
+ pacman -S timeshift -y
+ pacman -S cmus -y
 
 #yay AUR
 git clone "https://aur.archlinux.org/yay-bin.git"
-cd yay-bin || exit
-makepkg -si || exit
 
 #end
 echo "Instale o dnsmasq e habilite ou descomente domain-needed, bogus-priv e bind-interface em /etc/dnsmasq.conf | Reinicie o sistema, amigão"
