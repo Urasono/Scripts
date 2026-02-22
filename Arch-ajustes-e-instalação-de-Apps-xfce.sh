d6b08559b5aee75f51555f9c3a19232e9e7b6736
Urasono <89348450+Urasono@users.noreply.github.com>
2026-02-22 10:33:25 -0300

    Update Arch-ajustes-e-instalação-de-Apps-xfce.sh

--- Arch-ajustes-e-instalação-de-Apps-xfce.sh
+++ Arch-ajustes-e-instalação-de-Apps-xfce.sh
@@ -5,16 +5,14 @@
 whoami || exit
 
 #check updates
-pacman -Sy
-pacman -Syu
-pacman-key --refresh-keys
-pacman -Syu archlinux-keyring -y
+
+pacman -Syu && pacman -Sy --needed archlinux-keyring && pacman -Su
 
 #grub-config microcódigo
 pacman -S amd-ucode -y
 
 #Modo root
-update-grub || exit
+grub-mkconfig -o /boot/grub/grub.cfg || exit
 
 #set keyboard
 setxkbmap -model abnt2 -layout br
@@ -28,15 +26,12 @@ systemctl start paccache.timer || exit
 #update
 pacman -Sy
 
-#remoção da mitigação split-lock
-echo "kernel.split_lock_mitigate=0" > /etc/sysctl.d/99-splitlock.conf || exit
-
 #Earlyoom Daemon Linux
 pacman -S earlyoom -y
 systemctl enable earlyoom || exit
 systemctl start earlyoom || exit
-# echo ""EARLYOOM_ARGS="-r 0 -m 2 -M 256000 --prefer '^(Web Content|Isolated Web Co)$' --avoid '^(dnf|apt|pacman|rpm-ostree|packagekitd|gnome-shell|gnome-session-c|gnome-session-b|lightdm|sddm|sddm-helper|gdm|gdm-wayland-ses|gdm-session-wor|gdm-x-session|Xorg|Xwayland|systemd|systemd-logind|dbus-daemon|dbus-broker|cinnamon|cinnamon-sessio|kwin_x11|kwin_wayland|plasmashell|ksmserver|plasma_session|startplasma-way|sway|i3|xfce4-session|mate-session|marco|lxqt-session|openbox|cryptsetup)$" > /etc/default/earlyoom
-#systemctl restart earlyoom
+echo ""EARLYOOM_ARGS="-r 0 -m 2 -M 256000 --prefer '^(Web Content|Isolated Web Co)$' --avoid '^(dnf|apt|pacman|rpm-ostree|packagekitd|gnome-shell|gnome-session-c|gnome-session-b|lightdm|sddm|sddm-helper|gdm|gdm-wayland-ses|gdm-session-wor|gdm-x-session|Xorg|Xwayland|systemd|systemd-logind|dbus-daemon|dbus-broker|cinnamon|cinnamon-sessio|kwin_x11|kwin_wayland|plasmashell|ksmserver|plasma_session|startplasma-way|sway|i3|xfce4-session|mate-session|marco|lxqt-session|openbox|cryptsetup)$" > /etc/default/earlyoom
+systemctl restart earlyoom
 
 #shader booster
 echo "# enforce RADV vulkan implementation for AMD GPUs
@@ -87,7 +82,6 @@ pacman -S gdu -y
 pacman -S keepassxc -y
 pacman -S firefox -y
 pacman -S mpv -y
-pacman -S btop -y
 pacman -S gstreamer -y
 pacman -S gst-plugins-bad -y
 pacman -S gst-plugins-good -y
