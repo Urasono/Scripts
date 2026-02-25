42bd27b50ada1fb4acc1ea683ca96ce13c58ada2
Urasono <89348450+Urasono@users.noreply.github.com>
2026-02-25 07:34:19 -0300

    Update Arch-ajustes-e-instalação-de-Apps-xfce.sh

--- Arch-ajustes-e-instalação-de-Apps-xfce.sh
+++ Arch-ajustes-e-instalação-de-Apps-xfce.sh
@@ -130,8 +130,9 @@ pacman -S xfce4-goodies -y
 pacman -S bleachbit -y
 #pacman -S --needed bash systemd pacman pacman-contrib archlinux-contrib curl fakeroot htmlq diffutils hicolor-icon-theme python python-pyqt6 qt6-svg glib2 xdg-utils
 
-#Lidar com pacotes .pacnew
+#Lidar com pacotes .pacnew e pacotes órfãos
 pacdiff || exit
+pacman -Qdtq | pacman -Rns - || exit
 
 #yay AUR
 git clone "https://aur.archlinux.org/yay-bin.git"
