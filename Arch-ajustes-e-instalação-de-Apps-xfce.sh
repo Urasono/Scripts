279eaa2b809e6242047b6f847ffb177ad8c935d5
Urasono <89348450+Urasono@users.noreply.github.com>
2026-02-25 19:39:46 -0300

    Update Arch-ajustes-e-instalação-de-Apps-xfce.sh

--- Arch-ajustes-e-instalação-de-Apps-xfce.sh
+++ Arch-ajustes-e-instalação-de-Apps-xfce.sh
@@ -2,7 +2,8 @@
 #Ajustes e instalação de Apps xfce - Ajustes e instalações de aplicativos no Arch linux, porém, com base no sistema em Inglês focando no uso com o XFCE.
 
 #Elevação do usuário ao root (CUIDADO)
-whoami || exit
+set -euxo pipefail
+IFS=$'\n\t'
 
 #check updates
 pacman -Syu && pacman -Sy --needed archlinux-keyring --noconfirm
