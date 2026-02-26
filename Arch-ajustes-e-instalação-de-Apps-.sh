e689f4a3013ec3dc5640b8110c45618bb7c73910
Urasono <89348450+Urasono@users.noreply.github.com>
2026-02-26 08:27:10 -0300

    Update Arch-ajustes-e-instalação-de-Apps-.sh

--- Arch-ajustes-e-instalação-de-Apps-.sh
+++ Arch-ajustes-e-instalação-de-Apps-.sh
@@ -30,14 +30,14 @@ systemctl start paccache.timer
 #Earlyoom Daemon Linux
 pacman -S earlyoom --noconfirm
 systemctl enable earlyoom
-
 echo "# Default settings for earlyoom. This file is sourced by /bin/sh from
 # /etc/init.d/earlyoom or by systemd from earlyoom.service.
 
 # Available minimum memory 15% and free minimum swap 5%
-# EARLYOOM_ARGS="-m 15 -s 5"
+# EARLYOOM_ARGS=\"-m 15 -s 5\"
 
-EARLYOOM_ARGS="-r 0 -m 2 -M 256000 --prefer '^(Web Content|Isolated Web Co)$' --avoid '^(dnf|apt|pacman|rpm-ostree|packagekitd|gnome-shell|gnome-session-c|gnome-session-b|lightdm|sddm|sddm-helper|gdm|gdm-wayland-ses|gdm-session-wor|gdm-x-session|Xorg|Xwayland|systemd|systemd-logind|dbus-daemon|dbus-broker|cinnamon|cinnamon-sessio|kwin_x11|kwin_wayland|plasmashell|ksmserver|plasma_session|startplasma-way|sway|i3|xfce4-session|mate-session|marco|lxqt-session|openbox|cryptsetup)$'"" > /etc/default/earlyoom
+EARLYOOM_ARGS=\"-r 0 -m 2 -M 256000 --prefer '^(Web Content|Isolated Web Co)$' --avoid '^(dnf|apt|pacman|rpm-ostree|packagekitd|gnome-shell|gnome-session-c|gnome-session-b|lightdm|sddm|sddm-helper|gdm|gdm-wayland-ses|gdm-session-wor|gdm-x-session|Xorg|Xwayland|systemd|systemd-logind|dbus-daemon|dbus-broker|cinnamon|cinnamon-sessio|kwin_x11|kwin_wayland|plasmashell|ksmserver|plasma_session|startplasma-way|sway|i3|xfce4-session|mate-session|marco|lxqt-session|openbox|cryptsetup)$'\"" > /etc/default/earlyoom
+
 systemctl start earlyoom
 
 #Escalonador De Disco
